e81d619f0a99ae4c9e96bce2371159ceb5e17de7
DrDano <danielharned@gmail.com>
2021-07-08 15:36:06 -0700

    built code to draw a square 070821

--- main.py
+++ main.py
@@ -2,6 +2,23 @@
 from turtle import Turtle, Screen
 
 big_john_turtle = Turtle()
+big_john_turtle.shape("turtle")
+big_john_turtle.color("AntiqueWhite")
+big_john_turtle.pencolor("black")
+
+for _ in range(0, 4):
+    big_john_turtle.forward(100)
+    big_john_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
 screen = Screen()
 screen.exitonclick()
 
