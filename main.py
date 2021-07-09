d9e9133b01c4e6cc275d9bf8cf2f901e0200c852
DrDano <danielharned@gmail.com>
2021-07-08 21:11:37 -0700

    Dashed line challenge completed 070821

--- main.py
+++ main.py
@@ -6,19 +6,13 @@ big_john_turtle.shape("turtle")
 big_john_turtle.color("AntiqueWhite")
 big_john_turtle.pencolor("black")
 
-for _ in range(0, 4):
-    big_john_turtle.forward(100)
-    big_john_turtle.right(90)
-
-
-
-
-
-
-
+for _ in range(0, 25):
+    big_john_turtle.pendown()
+    big_john_turtle.forward(5)
+    big_john_turtle.penup()
+    big_john_turtle.forward(5)
 
 
 
 screen = Screen()
 screen.exitonclick()
-
