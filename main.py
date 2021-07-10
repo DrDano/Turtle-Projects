0bce32d34747d28605dbd0499b0ddc4355a5cd58
DrDano <danielharned@gmail.com>
2021-07-10 11:27:30 -0700

    Spirograph completed 071021

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 
 from turtle import Turtle, Screen
+import random
+
+screen = Screen()
 
 big_john_turtle = Turtle()
 big_john_turtle.shape("turtle")
 big_john_turtle.color("AntiqueWhite")
-big_john_turtle.pencolor("black")
+big_john_turtle.pensize(1)
+big_john_turtle.speed(20)
+Screen().colormode(cmode=255)
 
-for _ in range(0, 25):
-    big_john_turtle.pendown()
-    big_john_turtle.forward(5)
-    big_john_turtle.penup()
-    big_john_turtle.forward(5)
+heading = 2
 
 
+for _ in range(0, 90):
+    r = random.randint(100, 255)
+    g = random.randint(100, 200)
+    b = random.randint(100, 200)
+    big_john_turtle.pencolor(r, g, b)
+    big_john_turtle.setheading(heading)
+    heading += 4
+    big_john_turtle.circle(100)
 
-screen = Screen()
 screen.exitonclick()
