1940e42b0a816cfd8b25cca6a099f21ab3602c41
DrDano <danielharned@gmail.com>
2021-07-12 07:10:20 -0700

    First commit of turtle race project (Day 19 100DoC) 071221

---
+++ Day_19_etchasketch/Turtle_Race_main.py
@@ -0,0 +1,6 @@
+from turtle import Turtle, Screen
+
+ted = Turtle()
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput("Make your bet!", "Which turtle will win the race? Enter a color: ")
