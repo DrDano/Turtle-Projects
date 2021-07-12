d9404c48cc3d924032307396fa4677366a1d2573
DrDano <danielharned@gmail.com>
2021-07-12 14:41:02 -0700

    turtles line up at starting line (Day 19) 071221

--- Day_19_etchasketch/Turtle_Race_main.py
+++ Day_19_etchasketch/Turtle_Race_main.py
@@ -1,6 +1,29 @@
 from turtle import Turtle, Screen
 
-ted = Turtle()
+ted = Turtle(shape='turtle')
+lellow = Turtle(shape='turtle')
+keen = Turtle(shape='turtle')
+moo = Turtle(shape='turtle')
+poringe = Turtle(shape='turtle')
+
+turtles_list = (ted, lellow, keen, moo, poringe)
+
+ted.color('red')
+lellow.color('yellow')
+keen.color('green')
+moo.color('blue')
+poringe.color('orange')
+
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput("Make your bet!", "Which turtle will win the race? Enter a color: ")
+
+y_pos = 120
+for t in turtles_list:
+    t.penup()
+    t.goto(x=-230, y=y_pos)
+    y_pos -= 60
+
+
+
+screen.exitonclick()
